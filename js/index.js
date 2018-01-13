ecbed0652ff8910ad7ea4bed5c3d07be014d4558
xinyu05 <33844922+xinyu05@users.noreply.github.com>
2018-01-13 23:39:03 +0100

    major update
    update

--- js/index.js
+++ js/index.js
@@ -11,10 +11,24 @@ $(document).ready(function() {
     $('#emotibubbles-preview-img').removeClass('preview-image-selected');
   });
 
-  $('#whack-preview').hover(function() {
-    $('#whack-preview-img').addClass('preview-image-selected');
+  $('#sn-preview').hover(function() {
+    $('#sn-preview-img').addClass('preview-image-selected');
   }, function() {
-    $('#whack-preview-img').removeClass('preview-image-selected');
+    $('#sn-preview-img').removeClass('preview-image-selected');
+  });
+
+   $('#Supor-preview').hover(function() {
+    $('#Supor-preview-img').addClass('preview-image-selected');
+  }, function() {
+    $('#Supor-preview-img').removeClass('preview-image-selected');
+  });
+
+
+
+  $('#library-preview').hover(function() {
+    $('#library-preview-img').addClass('preview-image-selected');
+  }, function() {
+    $('#library-preview-img').removeClass('preview-image-selected');
   });
 
   $('#youtube-preview').hover(function() {
@@ -29,16 +43,18 @@ $(document).ready(function() {
     $('#microsoft-preview-img').removeClass('preview-image-selected');
   });
 
-  $('#emotibubbles-preview').click(function() {
-    window.location = 'projects/emotibubbles/home.html';
+  $('#library-preview').click(function() {
+    window.location = 'projects/library/home.html';
   });
-
-  $('#youtube-preview').click(function() {
-    window.location = 'projects/youtube/home.html';
+  $('#sn-preview').click(function() {
+    window.location = 'projects/sn/home.html';
   });
-
-  $('#microsoft-preview').click(function() {
-    window.location = 'projects/microsoft/home.html';
+    $('#library-preview').click(function() {
+    window.location = 'projects/library/home.html';
   });
+      $('#Supor-preview').click(function() {
+    window.location = 'projects/Supor/home.html';
+  });
+ 
 
 });
